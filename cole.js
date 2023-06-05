a1445d305a0cba175e48dce97c486fc66525f887
selevari <131004987+selevari@users.noreply.github.com>
2023-06-05 14:32:17 -0300

    Add files via upload

---
+++ cole.js
@@ -0,0 +1,28 @@
+var ColeGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+            title: "Esperando el cole luego de clase. 20:06hrs",
+            audio_url: "https://selevari.github.io/SonidosSantaFe/audio/cole.aac"
+        },
+        "geometry": {
+          "coordinates": [
+            -60.67252357350968,
+            -31.64032351542066
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
+
+  L.geoJSON(ColeGeoJSON, {
+    onEachFeature: function (feature, layer) {
+        var UniPopup =
+            "<h5>" + feature.properties.title + "</h5>" +
+            "<audio src = " + feature.properties.audio_url + " controls />"
+        layer.bindPopup(UniPopup)
+    }
+  }).addTo(mymap)
