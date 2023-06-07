a1be3aa10f6bde05f38562a3421a8f13013cc67f
selevari <131004987+selevari@users.noreply.github.com>
2023-06-07 19:43:53 -0300

    Add files via upload

---
+++ data/tren.js
@@ -0,0 +1,28 @@
+var TrenGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+            title: "Pasando por las antiguas vías del tren",
+            audio_url: "https://selevari.github.io//SonidosSantaFe/audio/tren.aac"
+        },
+        "geometry": {
+          "coordinates": [
+            -60.685909411544,
+            -31.639354769403255
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
+
+  L.geoJSON(TrenGeoJSON, {
+    onEachFeature: function (feature, layer) {
+        var trenPopup =
+            "<h7>" + feature.properties.title + "</h7>" +
+            "<audio src = " + feature.properties.audio_url + " controls />"
+        layer.bindPopup(trenPopup)
+    }
+  }).addTo(mymap)
