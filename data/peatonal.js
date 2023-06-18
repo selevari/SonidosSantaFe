5113c5258ee04bf1b23010e5ea6f98e842c55c38
selevari <131004987+selevari@users.noreply.github.com>
2023-06-18 18:13:59 -0300

    Add files via upload

---
+++ data/peatonal.js
@@ -0,0 +1,50 @@
+var PeatonalGeoJSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+            title: "Grabación de paisaje sonoro en el pasaje Peatonal",
+            audio_url: "https://selevari.github.io/SonidosSantaFe/audio/peatonal.mp3"
+        },
+        "geometry": {
+          "coordinates": [
+            [
+              -60.70663263289454,
+              -31.64679731688782
+            ],
+            [
+              -60.70696438593929,
+              -31.64800644120355
+            ],
+            [
+              -60.70737576540351,
+              -31.649149502743548
+            ],
+            [
+              -60.70772946913988,
+              -31.65034716635548
+            ],
+            [
+              -60.70806352266696,
+              -31.6514049919792
+            ],
+            [
+              -60.708397576195935,
+              -31.652513451022045
+            ]
+          ],
+          "type": "LineString"
+        }
+      }
+    ]
+  }
+
+  L.geoJSON(PeatonalGeoJSON, {
+    onEachFeature: function (feature, layer) {
+        var PeaPopup =
+            "<h3>" + feature.properties.title + "</h3>" +
+            "<audio src = " + feature.properties.audio_url + " controls />"
+        layer.bindPopup(PeaPopup)
+    }
+  }).addTo(mymap)
